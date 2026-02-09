9aaa83e9984f4e0e37c90adeb58475396539efd5
Noor <noor@example.com>
2026-02-09 00:15:18 -0800

    Finished register backend

--- Backend/main.py
+++ Backend/main.py
@@ -18,3 +18,12 @@ init_db()
 @app.get("/")
 def read_root():
     return {"message": "CareerCompass backend is working whoooo yayayayayaay w database is Ready kaman"}
+from fastapi import Form
+@app.post("/register")
+def register(name: str = Form(...), email: str = Form(...), grade_level: str = Form(...)):
+    conn = sqlite3.connect("careercompass.db")
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO students (name, email, grade_level) VALUES (?, ?, ?)", (name, email, grade_level))
+    conn.commit()
+    conn.close()
+    return {"message": f"Welcome {name}! You have been registered successfully."}
