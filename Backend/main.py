d5ebef1951898c1f886d2c18b78a68aab2c8f181
Noor <noor@example.com>
2026-02-08 23:52:52 -0800

    Added database setup

--- Backend/main.py
+++ Backend/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
-
+import sqlite3
 app = FastAPI()
-
+def init_db():
+    conn = sqlite3.connect("careercompass.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        email TEXT,
+        grade_level TEXT
+    )
+    """)
+    conn.commit()
+    conn.close()
+init_db()
 @app.get("/")
 def read_root():
-    return {"message": "CareerCompass (or website for student guider career) backend is working whoooo yayayayayaay"}
+    return {"message": "CareerCompass backend is working whoooo yayayayayaay w database is Ready kaman"}
